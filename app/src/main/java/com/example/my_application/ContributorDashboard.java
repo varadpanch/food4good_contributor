38548f7aa662e14f8883809103de1b2501c6e7f5
Varad Panch <varadpanch@Varads-MBP.lan>
2022-04-09 21:49:08 -0700

    Contributor Dashboard UI changes

--- app/src/main/java/com/example/my_application/ContributorDashboard.java
+++ app/src/main/java/com/example/my_application/ContributorDashboard.java
@@ -6,12 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ContributorDashboard extends AppCompatActivity {
 
-    Button rewards;
-    Button orders;
-    Button makeFoodAvailable;
+    ImageButton rewards;
+    ImageButton orders;
+    ImageButton makeFoodAvailable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
